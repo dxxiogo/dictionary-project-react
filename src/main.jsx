b124a4bc7de631cc65cf345fa004bd67a8238624
Diogo Santana <diogosantanafreitasuna@gmail.com>
2023-06-07 18:17:53 -0300

    Aplicando responsividade

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { ThemeProvider } from './contexts/ThemeContext.jsx'
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+// const router = RouterProvider([])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
